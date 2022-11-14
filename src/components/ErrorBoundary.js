6313cd30d89ee90184c51255e4acc3c5a0259f21
abhilasha <abhilasha15597@gmail.com>
2022-11-14 20:40:18 +0530

    new

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
  
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
